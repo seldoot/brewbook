5439ab742da91a6a22609d0d6704bc1490809b43
Todd Van Doren <tl.vandoren@gmail.com>
2016-03-22 18:30:08 -0700

    template controllers for recipes and brewers

--- app/controllers/brewers_controller.rb
+++ app/controllers/brewers_controller.rb
@@ -1,7 +1,16 @@
 class BrewersController < ApplicationController
+  
   def index
+  	@brewers=List.all
   end
 
   def show
+  	@brewer = Brewer.find(params[:id])
+  end
+
+  private
+  
+  def brewer_params
+  	params.require(:fname).permit(:lname, :brewing_interests)
   end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,43 @@
 class RecipesController < ApplicationController
   def index
-  end
+  	@recipes = List.all
+  	  end
 
   def show
+  	@recipe = Recipe.find(params[:id])
   end
+
+  def new
+  	@recipe = Recipe.new(:recipe_name => params[:id])
+  end
+
+  def create
+  	@recipe = Recipe.new(recipe_params)
+  	if @recipe.save
+  		flash[:notice] = "Your recipe has been saved."
+  		redirect_to @recipe
+  	else
+  		render :new
+  	end
+  end
+
+  def edit
+  	@recipe = Recipe.find(params[:id])
+  end
+
+  def update
+  	@recipe = List.find(params[:id])
+    if @recipe.update(recipe_params)
+     flash[:notice] = "recipe updated"
+     redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def recipe_params
+  	params.require(:recipe_name)
+  	.permit(:description, :brew_type, :brewer_id)
 end
